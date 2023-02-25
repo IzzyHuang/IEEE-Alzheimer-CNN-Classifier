557c9bc3da725d7401b943f86597423da36d7516
Akash Shroff <63399889+akashvshroff@users.noreply.github.com>
2023-02-24 22:32:10 -0600

    grouping images part 1

--- image_dir_walks.py
+++ image_dir_walks.py
@@ -8,10 +8,22 @@ def count_raw_data(path="D:\ADNI\RAW"):
         total += len(files)
     return total
 
+def get_category_info(dir="D:\ADNI\RAW",csv_path = "D:\ADNI\description.csv"):
+    category = {}
+    categories = ["MCI","AD","CN"]
+    for label in categories:
+        category[label] = 0
+    df = pd.read_csv(csv_path)
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            if "ADNI" not in file:
+                continue
+            id = file.split("_")[-1][:-4]
+            #row = df["Image Data ID"]
 
 if __name__ == "__main__":
     path = "D:\ADNI\RAW"
     csv_path = "D:\ADNI\description.csv"
     print(count_raw_data(path))
-    #get_category_info(path,csv_path)
+    get_category_info(path,csv_path)
 

bed82705b88f4aaedfdeb4a02dc45da136b0c067
Akash Shroff <63399889+akashvshroff@users.noreply.github.com>
2023-02-25 00:55:21 -0600

    grouping images final

--- image_dir_walks.py
+++ image_dir_walks.py
@@ -16,14 +16,25 @@ def get_category_info(dir="D:\ADNI\RAW",csv_path = "D:\ADNI\description.csv"):
     df = pd.read_csv(csv_path)
     for root, dirs, files in os.walk(dir):
         for file in files:
-            if "ADNI" not in file:
+            if "ADNI" not in file: #skip over any random files
                 continue
             id = file.split("_")[-1][:-4]
-            #row = df["Image Data ID"]
+            #print(f"id: {id}")
+            #print(f"file name: {file}")
+            try:
+                row = df.loc[df['Image Data ID'] == id] 
+                group = row.iloc[0]['Group']
+                try:
+                    category[group] += 1
+                except KeyError:
+                    raise(f"Group not in [AD, CN, MCI]: {group}")
+            except KeyError:
+                raise(f"File not found in csv: {file}.")
+    return category
 
 if __name__ == "__main__":
     path = "D:\ADNI\RAW"
     csv_path = "D:\ADNI\description.csv"
-    print(count_raw_data(path))
-    get_category_info(path,csv_path)
+    #print(count_raw_data(path))
+    #print(get_category_info(path,csv_path))
 

e642c73603fcd4d79d5b8baf71c5c52746044433
Akash Shroff <63399889+akashvshroff@users.noreply.github.com>
2023-03-03 17:51:05 -0600

    reading in csv

--- preprocess.py
+++ preprocess.py
@@ -12,12 +12,17 @@ SKULL_STRIPPED_DB = "D:\ADNI\SS"
 CLASS_FOLDERS = ['CN/', 'MCI/', 'AD/']
 ATLAS = "D:\ADNI\mn305_atlas.nii"
 
-# prepare and organize the images
+df = pd.read_csv(CSV)
+
+# register all images
 for subdir in DB_SUBFOLDERS:
     for path, dirs, files in os.walk(DATABASE + subdir):
         if files:
             for file in files:
+                id = file.split("_")[-1][:-4]
+                row = df.loc[df['Image Data ID'] == id] 
+                group = row.iloc[0]['Group']
                 try:
-                    register_and_save(file, path, ATLAS)
+                    register_and_save(file, path, label)
                 except RuntimeError:
                     print('Exception with', os.path.join(path, file))
